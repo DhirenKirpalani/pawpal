0e1b37402c8ad1e257729c7a88fd1310961f3700
Dhiren Kirpalani <dhirenkirpalani@Dhirens-MacBook-Pro.local>
2026-06-03 21:39:01 +0700

    Fix Facebook domain verification meta tag placement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="facebook-domain-verification" content="cf9fla32ce4ez60wzbr7z5kvxfvjyu" />
+      </head>
       <body>{children}</body>
     </html>
   );
